b27c872c0e52c3922e1ca7c210671a1d26e58eff
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2024-03-03 11:06:06 -0500

    attemp admin migration

---
+++ src/components/customerAdmin/accountDashboard.vue
@@ -0,0 +1,185 @@
+<template>
+    <div class="col-12 col-md-11 col justify-center row">
+<!--           <div class="col-12 col-md-10  justify-around" style="">
+            <div class="bg-grey-9 text-white col-12 ">
+              <div class="text-h6">Your Account Overview</div>
+            </div>
+  
+            <div class="col-12 col-md-10 justify-around row" style="">
+              <main-chat class="col-11 col-md-6" :chatID='accountInfo.orders[0].chatID' ></main-chat>
+             <div class="bg-primary text-white col-11 col-md-5 col">
+              <div>
+                <div class="text-h5 q-mb-lg" >Account Summary</div>
+                <div class="text-subtitle1">
+                  Bond Amount: {{ accountInfo.metaData.bondAmount }} XMR
+                </div>
+                 <div class="text-subtitle1" style="overflow-wrap: break-word;">
+                  Your XMR Refund Address: <br/>
+                  {{ accountInfo.metaData.refundAddress }}
+                </div>
+                <div class="text-subtitle1" style="overflow-wrap: break-word;">
+                  Anon Shop XMR Address: <br/>
+                  45Ag8YnqVAq2wwAYksNfzfUrWVixAcUiGG5iwLwrSgwzbv6JWLhqUbKZHfXJM2g6GLWKaSs1roApEdiLjkSUMbvQBoZANx6
+                </div>
+              </div>
+              <div>
+                
+                <div class="column" style="">
+                  <div class="col col-6" v-for="item in accountInfo.orders" :key="item.nickname">
+                    <div class="text-subtitle1 bg-black q-pa-md rounded-borders">
+                      <div class="text-h5 q-mb-lg">Order Info</div>
+                      <div>{{capitalizeWords(item.nickName)}} for {{ item.totalUSD }} USD</div>
+                      <div>
+                        <q-btn color="secondary" label="View Order Details" class="q-my-sm" 
+                        @click="openDialog()"/></div>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              <div v-if="admin">
+                <q-btn color="red" label="Delete Account" class="q-my-sm" 
+                        @click="deleteAccount"/>
+              </div>
+            </div> 
+            
+          </div>
+        </div> -->
+  
+  
+<!--         <q-dialog v-model="dialogOpen">
+        <div>
+          <q-card-section class=" bg-grey-9">
+            <div class="text-h6 text-center text-white">
+              Order: {{ accountInfo.orders[0].nickName }}</div>
+          </q-card-section>
+          <q-separator />
+  
+          <q-card-section class="q-pt-none">
+            <div class="text-subtitle1 q-pa-md rounded-borders">
+              
+            <q-card class="bg-grey-14 text-white q-pa-md">
+              <div>Total USD Sent: {{ accountInfo.orders[0].totalUSD }} USD</div>
+                <div class="text-h6">Items:</div>
+                <div v-for="value in accountInfo.orders[0]['itemList']" :key="value.link"
+                class="q-my-lg">
+                  <li>Link: <a :href="value.link">Link</a></li>
+                  <li>Cost: {{value.cost}}</li>
+                  <li>Quantity: {{ value.quantity }}</li>
+                  <li>Notes: {{ value.description }}</li>
+                </div>
+              </q-card>
+              <q-card class="bg-grey text-white q-pa-md q-my-md" 
+              v-if="accountInfo.orders[0].lockerZipcode !== undefined">
+                <div>
+                  Locker Zipcode: {{accountInfo.orders[0].lockerZipcode }}
+                </div>
+                <div>
+                  Locker Country: {{accountInfo.orders[0].country }}
+                </div>
+                <div>
+                  Locker Name: {{accountInfo.orders[0].lockerName }}
+                </div>
+              </q-card>
+  
+              <q-card class="bg-grey text-white q-pa-md q-my-md" 
+              v-if="accountInfo.orders[0].addressInfo !== undefined">
+                Address Info: <br/>
+                <li>Name: {{ accountInfo.orders[0].addressInfo.fullname }}</li>
+                <li>Street: {{ accountInfo.orders[0].addressInfo.streetAddress }}</li>
+                <li>Apt or Suite #: {{ accountInfo.orders[0].addressInfo.aptNumber }}</li>
+                <li>City: {{ accountInfo.orders[0].addressInfo.city }}</li>
+                <li>Zipcode: {{ accountInfo.orders[0].addressInfo.zipcode }}</li>
+                <li>Country: {{ accountInfo.orders[0].addressInfo.country }}</li>
+                Discount Chosen: {{ accountInfo.orders[0].discountPossible }}<br/>
+                <div  v-if="accountInfo.orders[0].discountPossible">Discount Percent: {{accountInfo.orders[0].discountPercent }} %</div>
+              </q-card>
+              <q-card class="bg-grey-14 text-white q-pa-md q-my-md" >
+              <div>Order Notes: {{accountInfo.orders[0].extraNotes }}</div>
+              <div>Total USD Sent: {{accountInfo.orders[0].totalUSD}}</div>
+              <div>Taxes Paid: {{accountInfo.orders[0].taxAmountUSD}}</div>
+              <div>Items Subtotal: {{accountInfo.orders[0].itemsSubtotal}}</div>
+              <div>Bond Paid: {{accountInfo.orders[0].bondUSD}}</div>
+              <div>Order Fee: {{accountInfo.orders[0].orderFeeUSD}}</div>
+              <div>Extra Amount: {{accountInfo.orders[0].extraAmountUSD}}</div>
+            </q-card>
+  
+            <q-card class="bg-grey text-white q-pa-md q-my-md" >
+              <div>Payment Rate:  {{accountInfo.orders[0].paymentInfo[0].rate}} XMR/USD</div>
+              <div>XMR Sent: {{accountInfo.orders[0].paymentInfo[0].paymentMethodPaid}} XMR</div>
+              <div>Payment Info:</div>
+                 <div> 
+                  <div v-for="value in accountInfo.orders[0].paymentInfo[0].payments" :key="value.id">
+                <li style="overflow-wrap: break-word;">Transaction ID: {{value.id}}</li>
+                <li >XMR Amount Recieved: {{value.value}}</li>
+              </div>
+              </div>
+            </q-card>
+            </div>
+          </q-card-section>
+  
+          <q-card-actions align="right">
+            <q-btn label="Close" color="red" v-close-popup />
+          </q-card-actions>
+        </q-card>        
+  
+      </q-dialog> -->
+
+
+      </div> 
+    </template>
+      
+  <script setup>
+  import { defineProps, toRef, ref, computed  } from "vue"
+  import mainChat from '@/components/customerAdmin/mainChat.vue'
+  import { useRouter } from 'vue-router'
+  import axios from 'axios'
+  const props = defineProps({
+    passphrase: { type: Array, required: true },
+    accountInfo: { type: Object, required: true }
+  })
+  const deleteCounter = ref(0)
+  /* const text = ref('')
+  const disableButtons = ref(false) */
+  const router = useRouter()
+  const dialogOpen = ref(false)
+  const accountInfo = toRef(props, 'accountInfo')
+  // const passphrase = toRef(props, 'passphrase')
+  function openDialog() {
+    dialogOpen.value = true
+  }
+  function capitalizeWords(str) {
+    return str
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+  async function deleteAccount() {
+    deleteCounter.value = deleteCounter.value + 1
+    if(deleteCounter.value < 6){ console.log('more delete presses needed'); return }
+    deleteCounter.value = 0
+    const results = await axios.post('/.netlify/functions/deleteAccount', 
+    { accountPhrase: accountInfo.value.passphrase, 
+      chatID: accountInfo.value.orders[0].chatID
+    })
+    console.log(results.data)
+  }
+
+
+/*   const admin = computed(() => {
+    const routeInfo = router.currentRoute.value
+    if (routeInfo.name === 'admin') {
+      return true
+    } else {
+      return false
+    }
+  }) */
+  
+  </script>
+  <style lang="sass" scoped>
+  .q-dialog
+    backdrop
+      backdrop-filter: blur(70px) !important
+  
+  </style>
+  
---
+++ src/components/customerAdmin/mainChat.vue
@@ -0,0 +1,162 @@
+<template>
+<!--   <div class="col-11 col-md-6 col justify-center column">
+    <div class="column justify-center" style=""> -->
+<!--         <q-card class="col-6 col q-my-lg text-center" style="">
+          <q-card-section class="bg-grey-9 text-white">
+            <div class="text-h6">Your Order Chat</div>
+          </q-card-section>
+          <q-separator />
+                  <q-card-section
+                      class="text-center col-12 text-center row  justify-center col"
+                      style="border-radius: 10px;"
+                    >
+                    <div class="row justify-center text-left overflow-auto" style="">
+                      <div style="width: 100%; height: 40vh;" class="overflow-auto scroll"
+                      ref="customChatDiv">
+                        <q-chat-message
+                          v-for="(message, index) in messageArray"
+                          :key="index"
+                          :text="[sanatizeHTMLString(message.message)]"
+                          :name="message.from"
+                          :sent="message.from === 'shopper'"
+                          :stamp="epochToLocalTime(message.sent)"
+                          :avatar="getAvatar(message.from)"
+                          text-html
+                          :text-color="getTextColor(message.from)"
+                          :bg-color="getBackgroundColor(message.from)"
+                        />
+                      </div>
+                    </div>
+          </q-card-section>
+          <q-separator />
+
+        <q-card-actions vertical>
+          <span class="text-subtitle1 text-weight-medium">Type Your Message:</span>
+          <div class="q-pa-md overflow-auto" style="">
+            <q-input
+              v-model="text"
+              filled
+              autogrow
+              class="overflow-auto scroll"
+              style="width: 100%;"
+            />
+          </div>
+          <div class="row justify-around q-gutter-y-md">
+      <div class="col-12 col-md-6 ">
+        <q-btn @click="sendMessage" :disable="disableButtons" color="deep-purple-5" icon="send">Send Message</q-btn>
+      </div>
+      <div class="col-12 col-md-6">
+        <q-btn @click="checkForMessages" :disable="disableButtons" color="indigo-5" icon="sync">Check For New Messages</q-btn>
+      </div>
+    </div>
+        </q-card-actions>
+        </q-card> -->
+  </template>
+    
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import anaonAvatar from "@/assets/chatAvatar.svg"
+import shopperAvatar from "@/assets/detective.svg"
+import sanitizeHtml from 'sanitize-html'
+import { defineProps, toRef, ref, computed, nextTick } from "vue"
+const router = useRouter()
+import axios from 'axios'
+const customChatDiv = ref(null);
+const props = defineProps({
+  chatID: { type: String, required: true }
+})
+const text = ref('')
+// const dialogOpen = ref(false)
+const disableButtons = ref(false)
+const chatID = toRef(props, 'chatID')
+const messageArray = ref([])
+function epochToLocalTime(epochTime){
+  const timeString = formatAMPM(new Date(epochTime)) + ' '
+  return timeString + new Date(epochTime).toLocaleString('en-us', { weekday:"long", month:"short", day:"numeric"})
+}
+function sanatizeHTMLString (dirty) {
+  const clean = sanitizeHtml(dirty, {
+  allowedTags: [ 'br', 'a' ],
+  allowedAttributes: {
+    'a': [ 'href' ]
+  }
+});
+  return clean
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+function getAvatar(sender) {
+  if (sender === 'dgoon') {
+    return anaonAvatar
+  }
+  if (sender === 'shopper') {
+    return shopperAvatar
+  }
+}
+function getTextColor(sender) {
+  if (sender === 'dgoon') {
+    return 'black'
+  }
+  if (sender === 'shopper') {
+    return 'white'
+  }
+}
+function getBackgroundColor(sender) {
+  if (sender === 'dgoon') {
+    return 'info'
+  }
+  if (sender === 'shopper') {
+    return 'grey-7'
+  }
+}
+async function sendMessage() {
+  if (text.value.length < 1) {
+    return
+  }
+  disableButtons.value = true
+  await sleep(1000)
+  disableButtons.value = false
+  const data = { chatID: chatID.value, message: text.value, sender: sender.value }
+  await axios.post('/.netlify/functions/sendMessage', data)
+  text.value = ''
+  await checkForMessages()
+}
+async function checkForMessages() {
+  disableButtons.value = true
+  await sleep(1000)
+  disableButtons.value = false
+  const results = await axios.post('/.netlify/functions/getMessageArray', { chatID: chatID.value })
+  messageArray.value =  results.data.messageArray
+  await nextTick()
+  const div = customChatDiv.value
+  console.log(div)
+  div.scrollTo({top: 9999999999999999999999999999999999999, behavior: "smooth"})
+}
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+const sender = computed(() => {
+  const routeInfo = router.currentRoute.value
+  if (routeInfo.name === 'admin') {
+    return 'dgoon'
+  } else {
+    return 'shopper'
+  }
+})
+onMounted(async () => {
+  const results = await axios.post('/.netlify/functions/getMessageArray', { chatID: chatID.value })
+  messageArray.value =  results.data.messageArray
+  await nextTick()
+  const div = customChatDiv.value
+  div.scrollTo({top: 99999999999999999999999999999999, behavior: "smooth"})
+})
+</script>
