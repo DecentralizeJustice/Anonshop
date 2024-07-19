d9c887ee69b5c685ffc8ccd9fec5248979244752
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2024-07-19 18:34:22 -0400

    fix email

--- netlify/functions/sendMeMessage.js
+++ netlify/functions/sendMeMessage.js
@@ -1,16 +1,19 @@
+const Joi = require("joi")
 const formData = require('form-data')
 const Mailgun = require('mailgun.js')
-const Joi = require("joi")
+
 const mailgun = new Mailgun(formData);
 const myEmail = process.env.myEmail
 const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY
 const DOMAIN = process.env.mailGunDomain
 const mg = mailgun.client({username: 'api', key: MAILGUN_API_KEY});
 exports.handler = async (event) => {
+  console.log('ran')
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" }
   }
+  console.log('ran')
   const params = event.body
   const parsed = JSON.parse(params)
 
@@ -20,7 +23,7 @@ exports.handler = async (event) => {
 
 
   const result = await mg.messages.create(DOMAIN, {
-    from: "Anon Server <me@samples.mailgun.org>",
+    from: `Anon Server <${process.env.myEmail}>`,
     to: [myEmail],
     subject: "New Anon Shop Message!",
     text: `${message}`
